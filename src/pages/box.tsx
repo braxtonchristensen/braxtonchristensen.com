9587ae96db3370cd22c071aa67154424b46754c9
Braxton M. Christensen <braxton.christensen@gmail.com>
2022-04-03 14:08:40 -0600

    feat/overhaul

---
+++ src/pages/box.tsx
@@ -0,0 +1,37 @@
+import dynamic from "next/dynamic";
+
+const Box = dynamic(() => import("@/components/canvas/Box"), {
+  ssr: false,
+});
+
+const DOM = () => {
+  return <></>;
+};
+
+const R3F = () => {
+  return (
+    <>
+      <Box route="/" />
+    </>
+  );
+};
+
+const Page = () => {
+  return (
+    <>
+      <DOM />
+      {/* @ts-ignore */}
+      <R3F r3f />
+    </>
+  );
+};
+
+export default Page;
+
+export async function getStaticProps() {
+  return {
+    props: {
+      title: "Box",
+    },
+  };
+}
